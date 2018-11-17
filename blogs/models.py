8abce9dfdf944c379da97eab1cd878f4ee13241a
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2018-11-17 14:38:21 +0100

    Create model Blog, migrate and connect with Django admin

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=50)
+    blog_description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{0}'.format(self.blog_title)
