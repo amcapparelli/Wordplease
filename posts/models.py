020c431e1af4b2384f306193fdd77950f4839506
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2018-11-17 20:11:57 +0100

    Create Post model and register into Django admin

---
+++ posts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+from blogs.models import Blog
+
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=150)
+    post_body = models.TextField()
+    date_published = models.DateTimeField(auto_now_add=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{0}'.format(self.post_title)
