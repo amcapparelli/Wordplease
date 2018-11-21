2d4e75799c9fab841f1efb5a68629783c5f1add4
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2018-11-21 22:28:10 +0100

    in user profile, user can now register his blog

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 
+from blogs.NewBlogForm import NewBlogForm
 from blogs.models import Blog
 from posts.models import Post
 
@@ -15,10 +16,20 @@ class BlogListView(View):
 class UserPageView(View):
     def get(self, request, username):
         user = get_object_or_404(User, username=username)
-        user_blog = Blog.objects.get(author=user)
-        posts = Post.objects.filter(blog=user_blog).order_by('-date_published')
-        context = {'posts': posts}
-        return render(request, 'blogs/user_page.html', context)
+        try:
+            user_blog = Blog.objects.get(author=user)
+            posts = Post.objects.filter(blog=user_blog).order_by('-date_published')
+            context = {'posts': posts}
+            return render(request, 'blogs/user_page.html', context)
+        except Blog.DoesNotExist:
+            form = NewBlogForm()
+            context = {'new_blog': form}
+            return render(request, 'blogs/create_blog.html', context)
 
-
+    def post(self, request, username):
+        blog = Blog(author=request.user)
+        form = NewBlogForm(request.POST, instance=blog)
+        if form.is_valid():
+            form.save()
+            return redirect('new_post')
 
