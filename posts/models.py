e9926cbf056c5d346bb379c6d97848c28bc8ddcf
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2018-11-25 16:48:40 +0100

    Create image field for posts model and add format to date_published field

--- posts/models.py
+++ posts/models.py
@@ -8,9 +8,10 @@ from categories.models import Category
 class Post(models.Model):
     post_title = models.CharField(max_length=150)
     post_body = models.TextField()
-    date_published = models.DateTimeField(auto_now_add=True)
+    date_published = models.DateTimeField('%m/%d/%Y %H:%M')
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     categories = models.ManyToManyField(Category)
+    image = models.URLField()
 
     def __str__(self):
         return '{0} ({1})'.format(self.post_title, self.blog)
